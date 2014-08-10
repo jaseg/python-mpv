9b68b0866ec49228ccaf3b455990a66838084082
jaseg <code@jaseg.net>
2014-08-10 23:25:25 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+setup(
+    name = 'mpv',
+    version = '0.1',
+    py_modules = ['mpv'],
+    description = ('A python interface to the mpv media player'),
+    url = 'https://github.com/jaseg/python-mpv',
+    author = 'jaseg',
+    author_email = 'github@jaseg.net',
+    license = 'AGPLv2'
+)
