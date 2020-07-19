632a87af49d9a568103e6f0013b5f8a7988b2fb3
jaseg <code@jaseg.net>
2020-07-19 21:13:36 +0200

    mpv.py: Update copyright date

--- mpv.py
+++ mpv.py
@@ -2,7 +2,7 @@
 # vim: ts=4 sw=4 et
 #
 # Python MPV library module
-# Copyright (C) 2017 Sebastian Götte <code@jaseg.net>
+# Copyright (C) 2017-2020 Sebastian Götte <code@jaseg.net>
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
 # Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
