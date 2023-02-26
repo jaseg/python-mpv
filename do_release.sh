d7664eb6716cb35a116b402da952dcb5ae0f01bd
jaseg <git@jaseg.de>
2023-02-26 15:17:38 +0100

    Fix release script for new packaging workflow

--- do_release.sh
+++ do_release.sh
@@ -10,8 +10,8 @@ if [ -n "$(git diff --name-only --cached)" ]; then
     exit 2
 fi
 
-sed -i "s/^\\(\\s*version\\s*=\\s*['\"]\\)[^'\"]*\\(['\"]\\s*,\\s*\\)$/\\1"$VER"\\2/" setup.py
-git add setup.py
+sed -i "s/^\\(\\s*version\\s*=\\s*['\"]\\)[^'\"]*\\(['\"]\\s*\\)$/\\1"$VER"\\2/" pyproject.toml
+git add pyproject.toml
 git commit -m "Version $VER" --no-edit
-env QUBES_GPG_DOMAIN=gpg git -c gpg.program=qubes-gpg-client -c user.signingkey=E36F75307F0A0EC2D145FF5CED7A208EEEC76F2D -c user.email=python-mpv@jaseg.de tag -s "v$VER" -m "Version $VER"
+git -c user.signingkey=E36F75307F0A0EC2D145FF5CED7A208EEEC76F2D -c user.email=python-mpv@jaseg.de tag -s "v$VER" -m "Version $VER"
 git push --tags origin
